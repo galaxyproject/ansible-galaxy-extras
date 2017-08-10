1b920813273898d26b97124748c114228371186d
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-08-10 09:04:33 +0200

    typo

--- tests/conditional_deps/test_script.sh
+++ tests/conditional_deps/test_script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo "Starting container"
 docker run -d -p 8080:80 -p 8021:21 -p 8022:22 \
---name galaxy_test_container \
 -e GALAXY_CONFIG_AUTH_CONFIG_FILE=config/auth_conf.xml \
 -v $PWD/tests/conditional_deps/auth_conf.xml:/galaxy-central/config/auth_conf.xml \
 --name galaxy-test \
@@ -10,11 +9,11 @@ docker ps
 echo "Waiting for container to load..."
 sleep 30
 echo "Check auth_conf.xml's presence"
-docker exec -u 1450 galaxy_test_container cat /galaxy-central/config/auth_conf.xml
+docker exec -u 1450 galaxy-test cat /galaxy-central/config/auth_conf.xml
 echo "Wait some more for the dependency to install"
 sleep 30
 echo "Testing presence of conditional dependency in virtual environment..."
-docker exec -u 1450 galaxy_test_container \
+docker exec -u 1450 galaxy-test \
 /galaxy_venv/bin/pip list --format=columns | grep python-ldap
 if [$? == 0 ]
   then echo "Conditional dependency loaded."
