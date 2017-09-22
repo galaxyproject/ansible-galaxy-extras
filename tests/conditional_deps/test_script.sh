9828ba0df07c18d88df740a6a5b7cc8421835d2a
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-09-22 17:24:28 +0200

    adapted test script

--- tests/conditional_deps/test_script.sh
+++ tests/conditional_deps/test_script.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 echo "Starting container"
-docker run -d -p 8080:80 -p 8021:21 -p 8022:22 \
+CONTAINER_ID=`docker run -d \
 -e GALAXY_CONFIG_AUTH_CONFIG_FILE=config/auth_conf.xml \
 -v $PWD/tests/conditional_deps/auth_conf.xml:/galaxy-central/config/auth_conf.xml \
---name galaxy-test \
-galaxy-docker/test
+galaxy-docker/test`
 docker ps
 echo "Waiting for container to load..."
 sleep 30
 echo "Check auth_conf.xml's presence"
-docker exec -u 1450 galaxy-test cat /galaxy-central/config/auth_conf.xml
+docker exec -u 1450 $CONTAINER_ID cat /galaxy-central/config/auth_conf.xml
 echo "Wait some more for the dependency to install"
 sleep 30
 echo "Testing presence of conditional dependency in virtual environment..."
-docker exec -u 1450 galaxy-test \
-/galaxy_venv/bin/pip list --format=columns | grep python-ldap
-if [$? == 0 ]
-  then echo "Conditional dependency loaded."
-  else echo "Conditional dependency not loaded!" && exit 1
+ldap_installed=`docker exec -u 1450 $CONTAINER_ID  \
+/galaxy_venv/bin/pip list --format=columns | grep python-ldap | wc -l`
+if [ $ldap_installed == 0 ]
+  then echo "Conditional dependency not loaded!" && exit 1
+  else echo "Conditional dependency loaded."
 fi
